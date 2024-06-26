eee073cf08606ebd7586961864904616b2fa8fac
Alvaro Segovia <alvarosego01@gmail.com>
2024-06-26 10:56:22 -0400

    Refactor redirect logic to include 'registro' page

--- functions/global/declarations.php
+++ functions/global/declarations.php
@@ -12,7 +12,7 @@ add_action('wp_enqueue_scripts', 'init_scripts_styles');
 
 
 function redirect_to_login_if_not_logged_in() {
-    if (!is_user_logged_in() && !is_page('wp-login.php')) {
+    if (!is_user_logged_in() && !is_page(array('wp-login.php', 'registro'))) {
         wp_redirect('/cursos-lui/wp-login.php');
         exit();
     }
